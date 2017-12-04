bdcb872f84c086477ec5c859d7c3ecf257be1dbf
hyu596 <jeffyu@MacBook-Pro-5.local>
2017-12-03 18:11:55 -0800

    Add Menu and GG Scenes

--- Assets/_Scripts/Menu/MouseHover.cs
+++ Assets/_Scripts/Menu/MouseHover.cs
@@ -2,25 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class MouseHover : MonoBehaviour {
 
-	private Text text;
+	private TextMesh text;
 
 	// Use this for initialization
 	void Start () {
-		text = GetComponent<Text> ();
+		text = GetComponent<TextMesh> ();
 		text.color = Color.white;
-
-		Debug.Log (transform.position.x);
 	}
-	
+
 	void OnMouseEnter(){
-		Debug.Log ("aaaa");
-		text.color = Color.blue;
+		text.color = Color.cyan;
 	}
-
+		
 	void OnMouseExit(){
 		text.color = Color.white;
+	}
+
+	void Update(){
+		if (Input.GetMouseButtonDown (0) && text.color == Color.cyan) {
+			if (this.tag == "start")
+				SceneManager.LoadScene (0);
+			else if (this.tag == "quit")
+				Application.Quit ();
+		}
 	}
 }
