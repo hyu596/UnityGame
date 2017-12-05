57a36971ab9e00336bf7aa5abe47235712fb8415
wang zipei <zipeiw@gmail.com>
2017-12-05 12:40:32 -0800

    add textmexhpro

--- Assets/_Scripts/Menu/MouseHover.cs
+++ Assets/_Scripts/Menu/MouseHover.cs
@@ -3,27 +3,34 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class MouseHover : MonoBehaviour {
 
 	private TextMesh text;
+   // private TextMeshPro textmeshPro;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
 		text = GetComponent<TextMesh> ();
-		text.color = Color.white;
+    //    textmeshPro = GetComponent<TextMeshPro>();
+
+    //    textmeshPro.color = Color.white;
+        text.color = Color.white;
 	}
 
 	void OnMouseEnter(){
 		text.color = Color.cyan;
-	}
+    //    textmeshPro.color = Color.cyan;
+    }
 		
 	void OnMouseExit(){
 		text.color = Color.white;
-	}
+     //   textmeshPro.color = Color.white;
+    }
 
 	void Update(){
-		if (Input.GetMouseButtonDown (0) && text.color == Color.cyan) {
+		if (Input.GetMouseButtonDown (0) ) {
 			if (this.tag == "start")
 				SceneManager.LoadScene (1);
 			else if (this.tag == "quit")
