211a0a722b1dab1f02d2405f152545bfa8eab158
wang zipei <zipeiw@gmail.com>
2017-12-24 20:41:14 -0800

    time bar

---
+++ Assets/_Scripts/TimerScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerScript : MonoBehaviour {
+    public Image timer;
+    public float increaseAmount;
+	// Use this for initialization
+
+	
+	// Update is called once per frame
+	void Update () {
+        timer.fillAmount = TimeManager.totalTime/5;
+        Debug.Log(TimeManager.totalTime / 5);
+	}
+}
